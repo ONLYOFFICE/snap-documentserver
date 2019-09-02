19187f5ce4e4b990f77f4ee0567f9fc3e163951f
Igor-Pigalitsyn <i.pigaliczin@yandex.ru>
2019-09-02 22:29:42 +0300

    mkdir on

--- config/nginx-start.sh
+++ config/nginx-start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# mkdir -p $SNAP_DATA/var/log/nginx/
-# mkdir -p $SNAP_DATA/var/cache/nginx/client_temp
-# mkdir -p $SNAP_DATA/var/log/onlyoffice/documentserver
-# touch $SNAP_DATA/var/log/onlyoffice/documentserver/nginx.error.log
-# touch $SNAP_DATA/var/log/nginx/access.log
-# touch $SNAP_DATA/var/log/nginx/error.log
+mkdir -p $SNAP_DATA/var/log/nginx/
+mkdir -p $SNAP_DATA/var/cache/nginx/client_temp
+mkdir -p $SNAP_DATA/var/log/onlyoffice/documentserver
+touch $SNAP_DATA/var/log/onlyoffice/documentserver/nginx.error.log
+touch $SNAP_DATA/var/log/nginx/access.log
+touch $SNAP_DATA/var/log/nginx/error.log
 $SNAP/bin/nginx
