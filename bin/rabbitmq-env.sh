d963b232328c33f74c44d99b7eeb41ea4753a3ad
Igor-Pigalitsyn <i.pigaliczin@yandex.ru>
2019-09-02 17:49:10 +0300

    add bin

---
+++ bin/rabbitmq-env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+RABBITMQ_MNESIA_BASE="$SNAP_DATA/var/lib/rabbitmq/mnesia"
+RABBITMQ_LOG_BASE="$SNAP_DATA/var/log/rabbitmq"
+RABBITMQ_GENERATED_CONFIG_DIR="$SNAP_DATA/var/lib/rabbitmq/config"
+RABBITMQ_SCHEMA_DIR="$SNAP_DATA/var/lib/rabbitmq/schema"
+RABBITMQ_CONF_ENV_FILE="$SNAP/rabbitmq-env.conf"
+
+. `dirname $0`/rabbitmq-env
