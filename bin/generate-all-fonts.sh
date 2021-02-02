0517cf284d54d6431a775f6f0a392674877e049f
Roman Demidov <roman.demidov@onlyoffice.com>
2021-02-02 15:50:05 +0300

    Fix #53: Snap auto update broke configuration (#55)

---
+++ bin/generate-all-fonts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+TOOLDIR="$SNAP/var/www/onlyoffice/documentserver/server/tools"
+INDIR="$SNAP/var/www/onlyoffice/documentserver"
+OUTDIR="$SNAP_DATA/var/www/onlyoffice/documentserver"
+
+mkdir -p $OUTDIR/server/FileConverter/bin
+mkdir -p $OUTDIR/fonts
+mkdir -p $OUTDIR/sdkjs/common/Images
+
+"$TOOLDIR/allfontsgen" --input="$INDIR/core-fonts;$SNAP/usr/share/fonts"  --allfonts-web="$OUTDIR/sdkjs/common/AllFonts.js"  --allfonts="$OUTDIR/server/FileConverter/bin/AllFonts.js"  --images="$OUTDIR/sdkjs/common/Images"   --selection="$OUTDIR/server/FileConverter/bin/font_selection.bin"  --output-web="$OUTDIR/fonts"  --use-system="true"
+
+mkdir -p $OUTDIR/sdkjs/slide/themes
+cp -R $INDIR/sdkjs/slide/themes_backup/src $OUTDIR/sdkjs/slide/themes
+
+"$TOOLDIR/allthemesgen" --converter-dir="$INDIR/server/FileConverter/bin" --src="$OUTDIR/sdkjs/slide/themes" --output="$OUTDIR/sdkjs/common/Images"
