cdd29153f147ed8b753f62f03bfc93f64edbb822
Roman Demidov <roman.demidov@onlyoffice.com>
2022-03-28 10:29:03 +0300

    Small changes

--- bin/documentserver-start.sh
+++ bin/documentserver-start.sh
@@ -40,11 +40,15 @@ fi
 LOOPBACK_ENABLED=$(snapctl get onlyoffice.loopback)
 NGINX_CONF_PATH="$SNAP_DATA/etc/onlyoffice/documentserver/nginx"
 if [ "${LOOPBACK_ENABLED}" == "true" ]; then
-    sed -i -r 's/ #(allow)/ \1/' $NGINX_CONF_PATH/ds.conf.tmpl $NGINX_CONF_PATH/ds-ssl.conf.tmpl
-    sed -i -r 's/ #(deny)/ \1/' $NGINX_CONF_PATH/ds.conf.tmpl $NGINX_CONF_PATH/ds-ssl.conf.tmpl
+    sed -i -r 's/ #(allow)/ \1/' $NGINX_CONF_PATH/ds.conf.tmpl \
+                                 $NGINX_CONF_PATH/ds-ssl.conf.tmpl
+    sed -i -r 's/ #(deny)/ \1/' $NGINX_CONF_PATH/ds.conf.tmpl \
+                                $NGINX_CONF_PATH/ds-ssl.conf.tmpl
 else
-    sed -i -r 's/ (allow)/ #\1/' $NGINX_CONF_PATH/ds.conf.tmpl $NGINX_CONF_PATH/ds-ssl.conf.tmpl
-    sed -i -r 's/ (deny)/ #\1/' $NGINX_CONF_PATH/ds.conf.tmpl $NGINX_CONF_PATH/ds-ssl.conf.tmpl
+    sed -i -r 's/ (allow)/ #\1/' $NGINX_CONF_PATH/ds.conf.tmpl \
+                                 $NGINX_CONF_PATH/ds-ssl.conf.tmpl
+    sed -i -r 's/ (deny)/ #\1/' $NGINX_CONF_PATH/ds.conf.tmpl \
+                                $NGINX_CONF_PATH/ds-ssl.conf.tmpl
 fi
 
 export LC_ALL=C.UTF-8
