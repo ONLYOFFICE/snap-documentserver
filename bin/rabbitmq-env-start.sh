8c539015eb31e635f96b10eb8ed3bd5a6617e4a0
Roman Demidov <roman.demidov@onlyoffice.com>
2022-12-29 11:05:54 +0300

    rabbit

--- bin/rabbitmq-env-start.sh
+++ bin/rabbitmq-env-start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+umask 0066
+
 RABBITMQ_MNESIA_BASE="$SNAP_DATA/var/lib/rabbitmq/mnesia"
 RABBITMQ_LOG_BASE="$SNAP_DATA/var/log/rabbitmq"
 RABBITMQ_GENERATED_CONFIG_DIR="$SNAP_DATA/var/lib/rabbitmq/config"
