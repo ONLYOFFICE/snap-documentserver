3542b2dcbe464ea1b1b5be9daa2277d2b1ca61ae
Roman Demidov <roman.demidov@onlyoffice.com>
2023-04-03 12:50:46 +0300

    Fix #61689: RabbitMQ no longer starts after refresh (#126)

--- bin/rabbitmq-env-start.sh
+++ bin/rabbitmq-env-start.sh
@@ -2,10 +2,10 @@
 
 umask 0066
 
-RABBITMQ_MNESIA_BASE="$SNAP_DATA/var/lib/rabbitmq/mnesia"
-RABBITMQ_LOG_BASE="$SNAP_DATA/var/log/rabbitmq"
-RABBITMQ_GENERATED_CONFIG_DIR="$SNAP_DATA/var/lib/rabbitmq/config"
-RABBITMQ_SCHEMA_DIR="$SNAP_DATA/var/lib/rabbitmq/schema"
-RABBITMQ_CONF_ENV_FILE="$SNAP/rabbitmq-env.conf"
+export RABBITMQ_MNESIA_BASE="$SNAP_DATA/var/lib/rabbitmq/mnesia"
+export RABBITMQ_LOG_BASE="$SNAP_DATA/var/log/rabbitmq"
+export RABBITMQ_GENERATED_CONFIG_DIR="$SNAP_DATA/var/lib/rabbitmq/config"
+export RABBITMQ_SCHEMA_DIR="$SNAP_DATA/var/lib/rabbitmq/schema"
+export RABBITMQ_CONF_ENV_FILE="$SNAP/rabbitmq-env.conf"
 
 . `dirname $0`/rabbitmq-env
