384242479233787a5a71f14b0c575f1f5c144b36
Roman Demidov <roman.demidov@onlyoffice.com>
2024-02-15 11:51:53 +0300

    Сhanges that do not affect functionality

--- bin/documentserver-start.sh
+++ bin/documentserver-start.sh
@@ -7,8 +7,6 @@ touch $SNAP_DATA/var/run/supervisord.pid
 
 mkdir -p $SNAP_DATA/var/log/onlyoffice/documentserver/docservice/
 mkdir -p $SNAP_DATA/var/log/onlyoffice/documentserver/converter/
-mkdir -p $SNAP_DATA/var/log/onlyoffice/documentserver/gc/
-mkdir -p $SNAP_DATA/var/log/onlyoffice/documentserver/metrics/
 mkdir -p $SNAP_DATA/var/log/onlyoffice/documentserver-example/
 mkdir -p $SNAP_DATA/var/lib/onlyoffice/documentserver-example/files/
 
@@ -16,25 +14,24 @@ touch $SNAP_DATA/var/log/onlyoffice/documentserver/docservice/out.log
 touch $SNAP_DATA/var/log/onlyoffice/documentserver/docservice/err.log
 touch $SNAP_DATA/var/log/onlyoffice/documentserver/converter/out.log
 touch $SNAP_DATA/var/log/onlyoffice/documentserver/converter/err.log
-touch $SNAP_DATA/var/log/onlyoffice/documentserver/gc/out.log
-touch $SNAP_DATA/var/log/onlyoffice/documentserver/gc/err.log
-touch $SNAP_DATA/var/log/onlyoffice/documentserver/metrics/out.log
-touch $SNAP_DATA/var/log/onlyoffice/documentserver/metrics/err.log
 touch $SNAP_DATA/var/log/onlyoffice/documentserver-example/out.log
 touch $SNAP_DATA/var/log/onlyoffice/documentserver-example/err.log
 
+SUPERVISOR_CONF_DIR=$SNAP_DATA/etc/supervisor/conf.d
+DS_CONF_DIR=$SNAP_DATA/etc/onlyoffice/documentserver
+
 EXAMPLE_ENABLED=$(snapctl get onlyoffice.example-enabled)
 if [ "${EXAMPLE_ENABLED}" == "true" ]; then
-    sed -i -e 's/autostart=false/autostart=true/'  $SNAP_DATA/etc/supervisor/conf.d/ds-example.conf
+    sed -i -e 's/autostart=false/autostart=true/'  $SUPERVISOR_CONF_DIR/ds-example.conf
 else
-    sed -i -e 's/autostart=true/autostart=false/'  $SNAP_DATA/etc/supervisor/conf.d/ds-example.conf
+    sed -i -e 's/autostart=true/autostart=false/'  $SUPERVISOR_CONF_DIR/ds-example.conf
 fi
 
 USE_UNAUTHORIZED_STORAGE_ENABLED=$(snapctl get onlyoffice.use-unautorized-storage)
 if [ "${USE_UNAUTHORIZED_STORAGE_ENABLED}" == "true" ]; then
-    sed -i -e 's/"rejectUnauthorized": true/"rejectUnauthorized": false/' /var/snap/onlyoffice-ds/current/etc/onlyoffice/documentserver/local.json
+    sed -i -e 's/"rejectUnauthorized": true/"rejectUnauthorized": false/' $DS_CONF_DIR/local.json
 else
-    sed -i -e 's/"rejectUnauthorized": false/"rejectUnauthorized": true/' /var/snap/onlyoffice-ds/current/etc/onlyoffice/documentserver/local.json
+    sed -i -e 's/"rejectUnauthorized": false/"rejectUnauthorized": true/' $DS_CONF_DIR/local.json
 fi
 
 export LC_ALL=C.UTF-8
