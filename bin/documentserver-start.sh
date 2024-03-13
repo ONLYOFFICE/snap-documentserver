11f0b895b55240b7bad7e9dfbbdbf63c91bb8dde
Roman Demidov <roman.demidov@onlyoffice.com>
2024-03-13 23:05:47 +0300

    Refactoring

--- bin/documentserver-start.sh
+++ bin/documentserver-start.sh
@@ -1,45 +1,43 @@
 #!/bin/bash
 
+LOG_DIR=$SNAP_DATA/var/log/onlyoffice/documentserver
+
 mkdir -p $SNAP_DATA/var/log/supervisor/
 mkdir -p $SNAP_DATA/var/run/
 touch $SNAP_DATA/var/log/supervisor/supervisord.log
 touch $SNAP_DATA/var/run/supervisord.pid
 
-mkdir -p $SNAP_DATA/var/log/onlyoffice/documentserver/docservice/
-mkdir -p $SNAP_DATA/var/log/onlyoffice/documentserver/converter/
-mkdir -p $SNAP_DATA/var/log/onlyoffice/documentserver-example/
-mkdir -p $SNAP_DATA/var/lib/onlyoffice/documentserver-example/files/
+mkdir -p $LOG_DIR/docservice/
+mkdir -p $LOG_DIR/converter/
+mkdir -p $LOG_DIR-example/
+mkdir -p $LOG_DIR-example/files/
 
-touch $SNAP_DATA/var/log/onlyoffice/documentserver/docservice/out.log
-touch $SNAP_DATA/var/log/onlyoffice/documentserver/docservice/err.log
-touch $SNAP_DATA/var/log/onlyoffice/documentserver/converter/out.log
-touch $SNAP_DATA/var/log/onlyoffice/documentserver/converter/err.log
-touch $SNAP_DATA/var/log/onlyoffice/documentserver-example/out.log
-touch $SNAP_DATA/var/log/onlyoffice/documentserver-example/err.log
+touch $LOG_DIR/docservice/out.log
+touch $LOG_DIR/docservice/err.log
+touch $LOG_DIR/converter/out.log
+touch $LOG_DIR/converter/err.log
+touch $LOG_DIR-example/out.log
+touch $LOG_DIR-example/err.log
 
 SUPERVISOR_CONF_DIR=$SNAP_DATA/etc/supervisor/conf.d
 DS_CONF_DIR=$SNAP_DATA/etc/onlyoffice/documentserver
 
 EXAMPLE_ENABLED=$(snapctl get onlyoffice.example-enabled)
 if [ "${EXAMPLE_ENABLED}" == "true" ]; then
-    sed -i -e 's/autostart=false/autostart=true/'  $SUPERVISOR_CONF_DIR/ds-example.conf
+    sed -i -e '/autostart/s/false/true/'  $SUPERVISOR_CONF_DIR/ds-example.conf
 else
-    sed -i -e 's/autostart=true/autostart=false/'  $SUPERVISOR_CONF_DIR/ds-example.conf
+    sed -i -e '/autostart/s/true/false/'  $SUPERVISOR_CONF_DIR/ds-example.conf
 fi
 
 USE_UNAUTHORIZED_STORAGE_ENABLED=$(snapctl get onlyoffice.use-unautorized-storage)
 if [ "${USE_UNAUTHORIZED_STORAGE_ENABLED}" == "true" ]; then
-    sed -i -e 's/"rejectUnauthorized": true/"rejectUnauthorized": false/' $DS_CONF_DIR/local.json
+    sed -i -e '/rejectUnauthorized/s/true/false/' $DS_CONF_DIR/local.json
 else
-    sed -i -e 's/"rejectUnauthorized": false/"rejectUnauthorized": true/' $DS_CONF_DIR/local.json
+    sed -i -e '/rejectUnauthorized/s/false/true/' $DS_CONF_DIR/local.json
 fi
 
 WOPI_ENABLED=$(snapctl get onlyoffice.wopi)
-if [ "${WOPI_ENABLED}" == "true" ]; then
-    sed -i -e 's/"enable": false/"enable": true/' $DS_CONF_DIR/local.json
-else
-    sed -i -e 's/"enable": true/"enable": false/' $DS_CONF_DIR/local.json
-fi
+sed -i -r 's/(.*"enable": ).*,/\1'$WOPI_ENABLED',/' $DS_CONF_DIR/local.json
 
 export LC_ALL=C.UTF-8
 
